8c52f835ba62ffd2f056ca3b9dd82f2d2772ecbf
George Onisiforou <george.onisiforou@vaslabs.io>
2022-07-26 12:22:00 +0300

    log Severity to Json

--- logger.test.ts
+++ logger.test.ts
@@ -1,14 +1,44 @@
-import { FunctionalLogger } from "./logger";
-
-
+import { FunctionalLogger, Severity } from "./logger";
 
 test('logs simple message in Json format', () =>{
  
     const logMessage = "Hello"
-    const expectedLogStatement = {message: logMessage}
+    
+    const expectedLogStatement = {message: logMessage, severity: Severity.INFO}
     const logger = new FunctionalLogger()
     
     const logOutput = logger.log(logMessage)
+
+    expect(logOutput).toEqual(expectedLogStatement)
+ 
+});
+
+
+/*
+    include severity
+    (verbosity level)
+
+    TRACE
+    DEBUG
+    INFO
+    WARN
+    ERROR
+
+    logger.log by default INFO
+    or pass explicitly
+    logger.log(message, severity)
+
+    severity is an enum of the above
+*/
+test('logs simple message with custom severity in Json format', () =>{
+ 
+    const logMessage = "Hello"
+    const severity = Severity.TRACE
+    
+    const expectedLogStatement = {message: logMessage, severity: severity}
+    const logger = new FunctionalLogger()
+    
+    const logOutput = logger.log(logMessage, severity)
 
     expect(logOutput).toEqual(expectedLogStatement)
  
--- logger.ts
+++ logger.ts
@@ -1,6 +1,26 @@
-export class FunctionalLogger{
-    log(logMessage: string) {
-       return ({message: logMessage})
-    }
-
+export enum Severity {
+    TRACE = "TRACE",
+    DEBUG = "DEBUG",
+    INFO = "INFO",
+    WARN = "WARN",
+    ERROR = "ERROR"
 }
+
+export class FunctionalLogger{
+    log(logMessage: string, severity: Severity = Severity.INFO) {
+        
+       return ({message: logMessage, severity: severity})
+    }
+}
+
+/*
+    enums are singleton types (single instance within the virtual machine process (nodejs runs in a v8 VM))
+
+    sealed trait Severity
+    case object INFO extends Severity
+
+    Set of Severity (DEBUG, INFO)
+
+    class A(Boolean, Boolean) => 2x2 = 4 // unlimited instances of A
+    Severity (size N) = N // N instances of Severity
+*/
